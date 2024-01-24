820ddb4cf0f7ea9f89e7143369287aba8b55d71f
rendy278 <114122187+rendy278@users.noreply.github.com>
2024-01-25 04:30:48 +0700

    Add files via upload

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Head = () => {
+  return (
+    <>
+      <section className="head">
+        <div className="container d_flex">
+          <div className="left row">
+            <i className="fa fa-phone"></i>
+            <label> +62 831 2289 5534</label>
+            <i className="fa fa-envelope"></i>
+            <label> kepolaumautauaja@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <label>IND</label>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Head;
